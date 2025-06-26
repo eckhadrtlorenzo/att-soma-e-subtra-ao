1e3bddbf4e457bd823b03ced02b2332ef5e0bc34
eckhadrtlorenzo <lorenzo.anzoategui@escola.pr.gov.br>
2025-06-26 08:36:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function soma(){
- 
   let numero_um = parseInt(document.getElementById('primeiro_nmr').value);
   let numero_dois = parseInt(document.getElementById('segundo_nmr').value);
   let soma = numero_um + numero_dois;
@@ -13,4 +12,46 @@ function subtrair(){
   let subtraçao = numero_um - numero_dois;
   
 document.getElementById('resultado').textContent = 'resultado: ' + subtraçao;
+}
+function multiplicar(){
+  let numero_um = parseInt(document.getElementById('primeiro_nmr').value);
+  let numero_dois = parseInt(document.getElementById('segundo_nmr').value);
+  let multiplicar = numero_um * numero_dois;
+  
+document.getElementById('resultado').textContent = 'resultado: ' + multiplicar;
+}
+function dividir(){
+  let numero_um = parseFloat(document.getElementById('primeiro_nmr').value);
+  let numero_dois = parseFloat(document.getElementById('segundo_nmr').value);
+ 
+  
+  if(numero_dois == 0){
+    alert("ERRO:nao é possivel dividir esse nmr por zero");
+    return;
+  }
+   let dividir = numero_um / numero_dois;
+  document.getElementById('resultado').textContent = 'resultado: ' + dividir;
+
+ 
+  
+}
+function comparaçao(){
+
+  let numero_um = parseInt(document.getElementById('primeiro_nmr').value);
+  let numero_dois = parseInt(document.getElementById('segundo_nmr').value);
+
+  if(numero_um > numero_dois){
+     document.getElementById('resultado').textContent = "o maior é " + numero_um;
+     return;
+    
+    }else if(numero_um < numero_dois){
+       document.getElementById('resultado').textContent = "o maior é" + numero_dois;
+       return;
+     
+      }else{
+       document.getElementById('resultado').textContent = "sao iguais";
+       
+    
+  
+}
 }
